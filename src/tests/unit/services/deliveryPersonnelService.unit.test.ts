cd222b7e66a22bdcb27ed8f8a0cf30714126c314
Caio César Silva <caio.c.silva@animaeducacao.com.br>
2022-02-28 17:34:16 -0300

    Unit testing fix

--- src/tests/unit/services/deliveryPersonnelService.unit.test.ts
+++ src/tests/unit/services/deliveryPersonnelService.unit.test.ts
@@ -4,24 +4,43 @@ import * as DeliveryPersonnelMocks from "../../mocks/deliveryPersonnel.mocks";
 describe("[Unit]", () => {
   describe("[Delivery Personnel]", () => {
     describe("[Get Delivery Personnel By Id]", () => {
-      const getDeliveryPersonnelServiceMock = jest
-        .spyOn(DeliveryPersonnelService, "getDeliveryPersonnel")
-        .mockResolvedValueOnce(DeliveryPersonnelMocks.deliveryPersonnelMock);
       describe("[Given the delivery personnel does exist]", () => {
         it("Should return a 200 status and the delivery personnel", async () => {
+          const getDeliveryPersonnelServiceMock = jest
+            .spyOn(DeliveryPersonnelService, "getDeliveryPersonnel")
+            .mockResolvedValue(DeliveryPersonnelMocks.deliveryPersonnelMock);
           const personnelId = DeliveryPersonnelMocks.deliveryPersonnelId;
           const deliveryPersonnel =
             await DeliveryPersonnelService.getDeliveryPersonnel({
               personnelId,
             });
 
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledWith({
+            personnelId,
+          });
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledTimes(1);
           expect(deliveryPersonnel).toEqual(
             DeliveryPersonnelMocks.deliveryPersonnelMock
+          );
+        });
+      });
+      describe("[Given the delivery personnel does NOT exist]", () => {
+        it("Should return null", async () => {
+          const getDeliveryPersonnelServiceMock = jest
+            .spyOn(DeliveryPersonnelService, "getDeliveryPersonnel")
+            .mockResolvedValue(null);
+          const personnelId = DeliveryPersonnelMocks.deliveryPersonnelId;
+          const personnel = await DeliveryPersonnelService.getDeliveryPersonnel(
+            {
+              personnelId,
+            }
           );
 
           expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledWith({
             personnelId,
           });
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledTimes(1);
+          expect(personnel).toBeNull();
         });
       });
     });
