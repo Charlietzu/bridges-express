d4bd50077bf00501bd26e2dd03747d21e60f41dc
Caio César Silva <caio.c.silva@animaeducacao.com.br>
2022-02-28 17:43:01 -0300

    Integration and unit testing for getting a delivery personnel

--- src/tests/integration/deliveryPersonnel.integration.test.ts
+++ src/tests/integration/deliveryPersonnel.integration.test.ts
@@ -7,19 +7,42 @@ const app = createServer();
 describe("[Integration]", () => {
   describe("[Delivery Personnel]", () => {
     describe("[Get Delivery Personnel By Id]", () => {
-      beforeEach(() => {
-        const getDeliveryPersonnelServiceMock = jest
-          .spyOn(DeliveryPersonnelService, "getDeliveryPersonnel")
-          .mockResolvedValueOnce(DeliveryPersonnelMocks.deliveryPersonnelMock);
-      });
       describe("[Given the delivery personnel does exist]", () => {
         it("Should return a 200 status and the delivery personnel", async () => {
+          const getDeliveryPersonnelServiceMock = jest
+            .spyOn(DeliveryPersonnelService, "getDeliveryPersonnel")
+            .mockResolvedValueOnce(
+              DeliveryPersonnelMocks.deliveryPersonnelMock
+            );
+          const deliveryPersonnelId =
+            DeliveryPersonnelMocks.deliveryPersonnelId;
           const { statusCode, body } = await supertest(app).get(
             `/api/delivery-personnels/${DeliveryPersonnelMocks.deliveryPersonnelId}`
           );
 
           expect(statusCode).toBe(200);
           expect(body).toEqual(DeliveryPersonnelMocks.deliveryPersonnelMock);
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledWith({
+            deliveryPersonnelId,
+          });
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledTimes(1);
+        });
+        it("Should return a 404 status", async () => {
+          const getDeliveryPersonnelServiceMock = jest
+            .spyOn(DeliveryPersonnelService, "getDeliveryPersonnel")
+            .mockResolvedValueOnce(null);
+          const deliveryPersonnelId =
+            DeliveryPersonnelMocks.deliveryPersonnelId;
+          const { statusCode, body } = await supertest(app).get(
+            `/api/delivery-personnels/${DeliveryPersonnelMocks.deliveryPersonnelId}`
+          );
+
+          expect(statusCode).toBe(404);
+          expect(body).toStrictEqual({});
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledWith({
+            deliveryPersonnelId,
+          });
+          expect(getDeliveryPersonnelServiceMock).toHaveBeenCalledTimes(1);
         });
       });
     });
