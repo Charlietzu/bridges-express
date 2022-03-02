7dcb4ac71c3a479dab53f1ed45d73f8ddc3e8da4
Caio César Silva <caio.c.silva@animaeducacao.com.br>
2022-03-02 10:50:50 -0300

    Environment fix

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
-import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
 import createServer from "./utils/server";
 
-const port: number = config.get<number>("port");
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+const uriServer = () => {
+  const environment: string = process.env.NODE_ENV?.trim() ?? "";
+  const host: string = process.env.HOST?.trim() ?? "";
+
+  if (!environment) {
+    logger.error("Environment is not defined");
+    process.exit(1);
+  }
+
+  if (!host) {
+    logger.error("Host is not defined");
+    process.exit(1);
+  }
+
+  logger.info(`Environment: ${environment}`);
+
+  if (environment == "development" || environment == "test") {
+    return `${host}:${port}`;
+  } else if (environment == "production") {
+    return host;
+  }
+};
 
 const app = createServer();
 
 app.listen(port, async () => {
-  logger.info(`Server is running at http://localhost:${port}`);
+  logger.info(`Server is running at ${uriServer()}`);
   await connect();
 });
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
-import config from "config";
 import logger from "./logger";
 
 async function connect() {
-    const dbUri: string = config.get<string>("dbUri");
-    try {
-        await mongoose.connect(dbUri);
-        logger.info("Connected to MongoDB");
-    } catch (err) {
-        logger.error(`Could not connect to MongoDB, error: ${err}`);
-        process.exit(1);
+  try {
+    let dbUri: string = "";
+
+    if (!process.env.DATABASE_URI) {
+      logger.error("Database URI is not defined");
+      process.exit(1);
     }
+
+    if (process.env.NODE_ENV !== "production") {
+      dbUri = process.env.DATABASE_URI;
+    } else {
+      //Logic to connect to production database
+    }
+    await mongoose.connect(dbUri);
+    logger.info("Connected to MongoDB");
+  } catch (err) {
+    logger.error(`Could not connect to MongoDB, error: ${err}`);
+    process.exit(1);
+  }
 }
 
 export default connect;
